0521f00525a01127d214d89bfd9ca1a9d5fed92a
tyanmahou <tyanmahou@gmail.com>
2025-01-02 22:09:35 +0900

    add token type

---
+++ Assets/PetitScript/Runtime/Lexer/TokenType.cs
@@ -0,0 +1,154 @@
+﻿
+
+namespace Petit.Lexer
+{
+    enum TokenType
+    {
+        /// <summary>
+        /// 無効値
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// バックスラッシュ
+        /// </summary>
+        BackSlash,
+
+        /// <summary>
+        /// (
+        /// </summary>
+        LParen,
+
+        /// <summary>
+        /// )
+        /// </summary>
+        RParen,
+
+        /// <summary>
+        /// 識別子
+        /// </summary>
+        Ident,
+
+        /// <summary>
+        /// 値
+        /// </summary>
+        Value,
+
+        /// <summary>
+        /// :
+        /// </summary>
+        Colon,
+
+        /// <summary>
+        /// .
+        /// </summary>
+        Dot,
+
+        /// <summary>
+        /// =
+        /// </summary>
+        Assign,
+
+        /// <summary>
+        /// !
+        /// </summary>
+        Not, 
+
+        /// <summary>
+        /// ==
+        /// </summary>
+        Equals,
+
+        /// <summary>
+        /// ===
+        /// </summary>
+        EqualsStrict,
+
+        /// <summary>
+        /// !=
+        /// </summary>
+        NotEquals,
+
+        /// <summary>
+        /// !==
+        /// </summary>
+        NotEqualsStrict,
+
+        /// <summary>
+        /// <
+        /// </summary>
+        LessThan,
+
+        /// <summary>
+        /// <=
+        /// </summary>
+        LessThanOrEquals,
+
+        /// <summary>
+        /// >
+        /// </summary>
+        GreaterThan,
+
+        /// <summary>
+        /// >=
+        /// </summary>
+        GreaterThanOrEquals,
+
+        /// <summary>
+        /// <=>
+        /// </summary>
+        Spaceship,
+
+        /// <summary>
+        /// &&
+        /// </summary>
+        LogicalAnd,
+        
+        /// <summary>
+        /// ||
+        /// </summary>
+        LogicalOr,
+
+        /// <summary>
+        /// +
+        /// </summary>
+        Add,
+
+        /// <summary>
+        /// -
+        /// </summary>
+        Sub,
+
+        /// <summary>
+        /// *
+        /// </summary>
+        Mul,
+
+        /// <summary>
+        /// /
+        /// </summary>
+        Div,
+
+        /// <summary>
+        /// %
+        /// </summary>
+        Mod,
+
+        /// <summary>
+        /// ?
+        /// </summary>
+        Question,
+
+        /// <summary>
+        /// 改行
+        /// </summary>
+        NewLine,
+
+        /// <summary>
+        /// エイリアス
+        /// </summary>
+        Plus = Add,
+        Minus = Sub,
+        Slash = Div,
+    }
+}
