1a19d0588a9ee9544961cabdf5c4192c918600f7
tyanmahou <tyanmahou@gmail.com>
2025-02-07 08:42:01 +0900

    refactor

--- Assets/PetitScript/Runtime/Core/Value.cs
+++ Assets/PetitScript/Runtime/Core/Value.cs
@@ -42,93 +42,93 @@ namespace Petit.Core
         }
         public Value(bool b)
         {
-            type = ValueType.Bool;
-            value = new ValueVariant();
-            value.BoolValue = b;
+            _type = ValueType.Bool;
+            _value = new ValueVariant();
+            _value.BoolValue = b;
         }
         public Value(int i)
         {
-            type = ValueType.Int;
-            value = new ValueVariant();
-            value.IntValue = i;
+            _type = ValueType.Int;
+            _value = new ValueVariant();
+            _value.IntValue = i;
         }
         public Value(float f)
         {
             if (!float.IsNaN(f))
             {
-                type = ValueType.Float;
-                value = new ValueVariant();
-                value.FloatValue = f;
+                _type = ValueType.Float;
+                _value = new ValueVariant();
+                _value.FloatValue = f;
             }
             else
             {
-                type = ValueType.NaN;
-                value = new ValueVariant();
-                value.FloatValue = float.NaN;
+                _type = ValueType.NaN;
+                _value = new ValueVariant();
+                _value.FloatValue = float.NaN;
             }
         }
         public Value(string s)
         {
-            type = ValueType.String;
-            value = new ValueVariant();
-            value.StringValue = s;
+            _type = ValueType.String;
+            _value = new ValueVariant();
+            _value.StringValue = s;
         }
         /// <summary>
         /// 無効値か
         /// </summary>
-        public bool IsInvalid => type == ValueType.Invalid;
+        public bool IsInvalid => _type == ValueType.Invalid;
 
         /// <summary>
         /// bool型か
         /// </summary>
-        public bool IsBool => type == ValueType.Bool;
+        public bool IsBool => _type == ValueType.Bool;
 
         /// <summary>
         /// int型か
         /// </summary>
-        public bool IsInt => type == ValueType.Int;
+        public bool IsInt => _type == ValueType.Int;
 
         /// <summary>
         /// Float型か
         /// </summary>
-        public bool IsFloat => type == ValueType.Float;
+        public bool IsFloat => _type == ValueType.Float;
 
         /// <summary>
         /// string型か
         /// </summary>
-        public bool IsString => type == ValueType.String;
+        public bool IsString => _type == ValueType.String;
 
         /// <summary>
         /// NaNか
         /// </summary>
-        public bool IsNaN => type == ValueType.NaN;
+        public bool IsNaN => _type == ValueType.NaN;
         public readonly bool ToBool()
         {
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue;
+                    return _value.BoolValue;
                 case ValueType.Int:
-                    return value.IntValue != 0;
+                    return _value.IntValue != 0;
                 case ValueType.Float:
-                    return value.FloatValue != 0;
+                    return _value.FloatValue != 0;
                 case ValueType.String:
-                    return !string.IsNullOrEmpty(value.StringValue);
+                    return !string.IsNullOrEmpty(_value.StringValue);
             }
             return false;
         }
         public readonly int ToInt()
         {
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue ? 1 : 0;
+                    return _value.BoolValue ? 1 : 0;
                 case ValueType.Int:
-                    return value.IntValue;
+                    return _value.IntValue;
                 case ValueType.Float:
-                    return (int)value.FloatValue;
+                    return (int)_value.FloatValue;
                 case ValueType.String:
-                    if (float.TryParse(value.StringValue, out float strParseValue))
+                    if (float.TryParse(_value.StringValue, out float strParseValue))
                     {
                         return (int)strParseValue;
                     }
@@ -138,16 +138,16 @@ namespace Petit.Core
         }
         public readonly float ToFloat()
         {
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue ? 1.0f : 0.0f;
+                    return _value.BoolValue ? 1.0f : 0.0f;
                 case ValueType.Int:
-                    return (float)value.IntValue;
+                    return (float)_value.IntValue;
                 case ValueType.Float:
-                    return value.FloatValue;
+                    return _value.FloatValue;
                 case ValueType.String:
-                    if (float.TryParse(value.StringValue, out float strParseValue))
+                    if (float.TryParse(_value.StringValue, out float strParseValue))
                     {
                         return strParseValue;
                     }
@@ -162,16 +162,16 @@ namespace Petit.Core
             const string trueStr = "true";
             const string falseStr = "false";
             const string NaNStr = "NaN";
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue ? trueStr : falseStr;
+                    return _value.BoolValue ? trueStr : falseStr;
                 case ValueType.Int:
-                    return value.IntValue.ToString();
+                    return _value.IntValue.ToString();
                 case ValueType.Float:
-                    return value.FloatValue.ToString();
+                    return _value.FloatValue.ToString();
                 case ValueType.String:
-                    return value.StringValue;
+                    return _value.StringValue;
                 case ValueType.NaN:
                     return NaNStr;
             }
@@ -179,7 +179,7 @@ namespace Petit.Core
         }
         public bool Equals(Value other)
         {
-            if (type != other.type)
+            if (_type != other._type)
             {
                 return false;
             }
@@ -187,16 +187,16 @@ namespace Petit.Core
             {
                 return false;
             }
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue == other.value.BoolValue;
+                    return _value.BoolValue == other._value.BoolValue;
                 case ValueType.Int:
-                    return value.IntValue == other.value.IntValue;
+                    return _value.IntValue == other._value.IntValue;
                 case ValueType.Float:
-                    return value.FloatValue == other.value.FloatValue;
+                    return _value.FloatValue == other._value.FloatValue;
                 case ValueType.String:
-                    return value.StringValue == other.value.StringValue;
+                    return _value.StringValue == other._value.StringValue;
             }
             return true;
         }
@@ -262,18 +262,18 @@ namespace Petit.Core
             {
                 return false;
             }
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue == other.ToBool();
+                    return _value.BoolValue == other.ToBool();
                 case ValueType.Int:
-                    return value.IntValue == other.ToInt();
+                    return _value.IntValue == other.ToInt();
                 case ValueType.Float:
-                    return value.FloatValue == other.ToFloat();
+                    return _value.FloatValue == other.ToFloat();
                 case ValueType.String:
-                    return value.StringValue == other.ToString();
+                    return _value.StringValue == other.ToString();
             }
-            return type == other.type;
+            return _type == other._type;
         }
 
         /// <summary>
@@ -291,16 +291,16 @@ namespace Petit.Core
         }
         public override int GetHashCode()
         {
-            switch (type)
+            switch (_type)
             {
                 case ValueType.Bool:
-                    return value.BoolValue.GetHashCode();
+                    return _value.BoolValue.GetHashCode();
                 case ValueType.Int:
-                    return value.IntValue.GetHashCode();
+                    return _value.IntValue.GetHashCode();
                 case ValueType.Float:
-                    return value.FloatValue.GetHashCode();
+                    return _value.FloatValue.GetHashCode();
                 case ValueType.String:
-                    return value.StringValue.GetHashCode();
+                    return _value.StringValue.GetHashCode();
                 case ValueType.NaN:
                     return float.NaN.GetHashCode();
             }
@@ -363,30 +363,30 @@ namespace Petit.Core
             }
             if (a.IsBool && b.IsBool)
             {
-                return a.value.BoolValue.CompareTo(b.value.BoolValue);
+                return a._value.BoolValue.CompareTo(b._value.BoolValue);
             }
             if (a.IsInt && b.IsInt)
             {
-                return a.value.IntValue.CompareTo(b.value.IntValue);
+                return a._value.IntValue.CompareTo(b._value.IntValue);
             }
             if (a.IsFloat && b.IsFloat)
             {
-                return a.value.FloatValue.CompareTo(b.value.FloatValue);
+                return a._value.FloatValue.CompareTo(b._value.FloatValue);
             }
             if (a.IsString && b.IsString)
             {
-                return a.value.StringValue.CompareTo(b.value.StringValue);
+                return a._value.StringValue.CompareTo(b._value.StringValue);
             }
             bool stringCompare = false;
             float aValue = 0.0f;
             float bValue = 0.0f;
             if (a.IsString)
             {
-                if (bool.TryParse(a.value.StringValue, out bool bo))
+                if (bool.TryParse(a._value.StringValue, out bool bo))
                 {
                     aValue = bo ? 1.0f : 0.0f;
                 }
-                else if (float.TryParse(a.value.StringValue, out float f))
+                else if (float.TryParse(a._value.StringValue, out float f))
                 {
                     aValue = f;
                 }
@@ -401,11 +401,11 @@ namespace Petit.Core
             }
             if (b.IsString)
             {
-                if (bool.TryParse(b.value.StringValue, out bool bo))
+                if (bool.TryParse(b._value.StringValue, out bool bo))
                 {
                     bValue = bo ? 1.0f : 0.0f;
                 }
-                else if (float.TryParse(b.value.StringValue, out float f))
+                else if (float.TryParse(b._value.StringValue, out float f))
                 {
                     bValue = f;
                 }
@@ -466,7 +466,7 @@ namespace Petit.Core
         }
         public static Value operator +(in Value a)
         {
-            switch (a.type)
+            switch (a._type)
             {
                 case ValueType.Bool:
                     return new Value(a.ToInt());
@@ -475,15 +475,15 @@ namespace Petit.Core
                 case ValueType.Float:
                     return a;
                 case ValueType.String:
-                    if (bool.TryParse(a.value.StringValue, out bool bo))
+                    if (bool.TryParse(a._value.StringValue, out bool bo))
                     {
                         return new Value(bo ? 1 : 0);
                     }
-                    else if (int.TryParse(a.value.StringValue, out int i))
+                    else if (int.TryParse(a._value.StringValue, out int i))
                     {
                         return new Value(i);
                     }
-                    else if (float.TryParse(a.value.StringValue, out float f))
+                    else if (float.TryParse(a._value.StringValue, out float f))
                     {
                         return new Value(f);
                     }
@@ -496,7 +496,7 @@ namespace Petit.Core
         }
         public static Value operator -(in Value a)
         {
-            switch (a.type)
+            switch (a._type)
             {
                 case ValueType.Bool:
                     return new Value(-a.ToInt());
@@ -505,15 +505,15 @@ namespace Petit.Core
                 case ValueType.Float:
                     return new Value(-a.ToFloat());
                 case ValueType.String:
-                    if (bool.TryParse(a.value.StringValue, out bool bo))
+                    if (bool.TryParse(a._value.StringValue, out bool bo))
                     {
                         return new Value(bo ? -1 : 0);
                     }
-                    else if (int.TryParse(a.value.StringValue, out int i))
+                    else if (int.TryParse(a._value.StringValue, out int i))
                     {
                         return new Value(-i);
                     }
-                    else if (float.TryParse(a.value.StringValue, out float f))
+                    else if (float.TryParse(a._value.StringValue, out float f))
                     {
                         return new Value(-f);
                     }
@@ -571,17 +571,17 @@ namespace Petit.Core
             }
             if (a.IsString)
             {
-                if (bool.TryParse(a.value.StringValue, out bool bo))
+                if (bool.TryParse(a._value.StringValue, out bool bo))
                 {
                     aiValue = bo ? 1 : 0;
                     afValue = aiValue;
                 }
-                else if (int.TryParse(a.value.StringValue, out int i))
+                else if (int.TryParse(a._value.StringValue, out int i))
                 {
                     aiValue = i;
                     afValue = aiValue;
                 }
-                else if (float.TryParse(a.value.StringValue, out float f))
+                else if (float.TryParse(a._value.StringValue, out float f))
                 {
                     floatOp = true;
                     afValue = f;
@@ -603,17 +603,17 @@ namespace Petit.Core
             }
             if (b.IsString)
             {
-                if (bool.TryParse(b.value.StringValue, out bool bo))
+                if (bool.TryParse(b._value.StringValue, out bool bo))
                 {
                     biValue = bo ? 1 : 0;
                     bfValue = biValue;
                 }
-                else if (int.TryParse(b.value.StringValue, out int i))
+                else if (int.TryParse(b._value.StringValue, out int i))
                 {
                     biValue = i;
                     bfValue = biValue;
                 }
-                else if (float.TryParse(b.value.StringValue, out float f))
+                else if (float.TryParse(b._value.StringValue, out float f))
                 {
                     floatOp = true;
                     bfValue = f;
@@ -798,7 +798,7 @@ namespace Petit.Core
             public string StringValue;
         }
 
-        private readonly ValueType type;
-        private readonly ValueVariant value;
+        private readonly ValueType _type;
+        private readonly ValueVariant _value;
     }
 }
