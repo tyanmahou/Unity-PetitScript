0f0b435ccecde4f2c6b2986fe06410f335544275
tyanmahou <tyanmahou@gmail.com>
2025-02-14 01:51:59 +0900

    add switch test

--- Assets/PetitScript/Tests/Core/InterpreterTest.cs
+++ Assets/PetitScript/Tests/Core/InterpreterTest.cs
@@ -132,6 +132,59 @@ else
             }
         }
         [Test]
+        public void TestSwitch()
+        {
+            string code = @"
+text="""";
+switch(a) {
+case 0:
+   text += ""0"";
+case  1:
+   text += ""1"";
+case  2:
+case  3:
+   text += ""23"";
+　break;    
+default:
+   text += ""d"";
+case  4:
+   text += ""4"";
+   break;
+}
+return text;
+";
+            {
+                var vars = new Variables();
+                vars.Set("a", 0);
+                RunString(code, "0123", vars);
+            }
+            {
+                var vars = new Variables();
+                vars.Set("a", 1);
+                RunString(code, "123", vars);
+            }
+            {
+                var vars = new Variables();
+                vars.Set("a", 2);
+                RunString(code, "23", vars);
+            }
+            {
+                var vars = new Variables();
+                vars.Set("a", 3);
+                RunString(code, "23", vars);
+            }
+            {
+                var vars = new Variables();
+                vars.Set("a", 4);
+                RunString(code, "4", vars);
+            }
+            {
+                var vars = new Variables();
+                vars.Set("a", 5);
+                RunString(code, "d4", vars);
+            }
+        }
+        [Test]
         public void TestFunc()
         {
             {
